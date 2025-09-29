27da5df050ceecf4318a2d4ad88f53159d1afade
Chil's Coding World <76067750+chilkwon@users.noreply.github.com>
2025-09-29 17:21:00 +0000

    added two repositories

---
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
@@ -0,0 +1,6 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.explorecalijpa.model.Tour;
+
+public interface TourRepository extends JpaRepository<Tour, Integer> {
+
+}
